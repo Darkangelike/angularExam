f6c00fefcab44e1b1519ba84ff051e1c531b08dd
Isabelle <isabelle.tieu@gmail.com>
2021-12-10 11:10:01 +0100

    Exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularExam';
+  @Input() stat2: Statistique = new Statistique(
+    'fghi',
+    'French people who can speak English',
+    '4%'
+  );
+
+  @Input()
+  stat1: Statistique = new Statistique('abcde', 'Women in web', '5.31%');
 }
